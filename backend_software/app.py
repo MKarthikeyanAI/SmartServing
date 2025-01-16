cfde6194886d611f4a3d10cb2752635d0f6b4022
MKarthikeyanAI <mkarthikeyan00100010@gmail.com>
2025-01-16 13:00:04 +0530

    changed app.py to deployment server

--- backend_software/app.py
+++ backend_software/app.py
@@ -516,12 +516,12 @@ def login():
 
     
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
-
 # if __name__ == '__main__':
-#     port = int(os.environ.get('PORT', 5000))  # Get the PORT from environment, default to 5000
-#     app.run(host='0.0.0.0', port=port, debug=True)
+#     app.run(debug=True)
+
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # Get the PORT from environment, default to 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
 
